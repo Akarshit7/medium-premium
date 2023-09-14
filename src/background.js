e437033947e176345a12f25c97cbe776188dcec6
Akarshit7 <akarshitbatra7@gmail.com>
2023-09-14 18:32:15 +0530

    fixed some typos

--- src/background.js
+++ src/background.js
@@ -12,7 +12,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
       }
       console.log(response.anchorText);
       if (response.anchorText) {
-        // Redirect the tab to the extracted URL
+      
         chrome.tabs.update(tabId, { url: response.anchorText });
       } else {
         console.log("No anchor text found.");
@@ -22,20 +22,20 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 });
 
 chrome.action.onClicked.addListener(function (tab) {
-  // Create a new tab with the Google Web Cache URL
+  
   chrome.tabs.create({
     url: `http://webcache.googleusercontent.com/search?q=cache:${URL}`,
   }, function (createdTab) {
-    // Send a message to the content script to get HTML content
+    
   });
 });
 
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-  // activeInfo.tabId contains the ID of the newly activated tab
+  
   const tabId = activeInfo.tabId;
 
-  // Get the URL of the currently active tab
+ 
   chrome.tabs.get(tabId, function(tab) {
     if (chrome.runtime.lastError) {
       console.error(chrome.runtime.lastError);
@@ -44,7 +44,7 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 
     URL = tab.url;
     console.log(URL);
-    // Now, you can use `currentTabUrl` as needed
+  
   
   });
 });
